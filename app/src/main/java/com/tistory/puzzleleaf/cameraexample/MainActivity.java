70276777485a75e751ff8c8998fa7b769c7cc072
PuzzleLeaf <cmtyx2@naver.com>
2017-08-04 18:22:55 +0900

    first commit

---
+++ app/src/main/java/com/tistory/puzzleleaf/cameraexample/MainActivity.java
@@ -0,0 +1,130 @@
+package com.tistory.puzzleleaf.cameraexample;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v4.os.EnvironmentCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView image;
+    private String imgPath = "";
+    Uri uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setPermission();
+        image = (ImageView)findViewById(R.id.imageView);
+
+        Button button = (Button)findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCamera();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            switch (requestCode) {
+                case 100:
+                    cropImage();
+                    break;
+                case 200:
+                    Glide.with(getApplicationContext()).load(uri).listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            File temp  = new File(imgPath);
+                            temp.delete();
+                            return false;
+                        }
+                    }).into(image);
+
+                    break;
+            }
+        }
+        else{
+            Log.d("qwe",data.toString());
+            Log.d("qwe", "Error" + String.valueOf(requestCode) + " " + String.valueOf(resultCode));
+        }
+    }
+
+    private void showCamera(){
+        uri = getFileUri();
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+        startActivityForResult(intent,100);
+
+    }
+
+    private void cropImage() {
+        Intent cropIntent = new Intent("com.android.camera.action.CROP");
+        cropIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        cropIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        //indicate image type and Uri of image
+        cropIntent.setDataAndType(uri, "image/*");
+        //set crop properties
+        cropIntent.putExtra("crop", "true");
+        cropIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        startActivityForResult(cropIntent, 200);
+    }
+
+    private Uri getFileUri(){
+        File dir = new File(this.getFilesDir(),"img");
+        if(!dir.exists()) {
+            dir.mkdir();
+        }
+        File file = new File(dir, System.currentTimeMillis() + "png");
+        imgPath = file.getAbsolutePath();
+
+        return FileProvider.getUriForFile(this, "com.tistory.puzzleleaf.cameraexample", new File(imgPath));
+    }
+
+    private void setPermission(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED||
+                ActivityCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, 1);
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+            return;
+        }
+    }
+
+}
